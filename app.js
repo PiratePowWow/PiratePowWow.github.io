02fe1ac10b276a632cce4356eb6977fcd311ac61
PiratePowWow <Thi3v3ry>
2016-05-04 16:01:38 -0400

    Resume Added

--- app.js
+++ app.js
@@ -26,3 +26,24 @@ $(function() {
         }
     });
 });
+function scrollToElement(e, t, n) {
+    t = typeof t != "undefined" ? t : 1e3;
+    n = typeof n != "undefined" ? n : 0;
+    element = $(e);
+    offset = element.offset();
+    offsetTop = offset.top + n;
+    $("html, body").animate({
+        scrollTop: offsetTop
+    }, t)
+}
+$(document).ready(function() {
+    $("a.toprojects").click(function() {
+        scrollToElement("#projects")
+    });
+    $("a.toservices").click(function() {
+        scrollToElement("#skills")
+    });
+    $("a.tocontact").click(function() {
+        scrollToElement("#contactInfo")
+    });
+});
